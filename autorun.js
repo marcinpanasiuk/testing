0982e73ca4267c596ab8504628ad69d9f944963d
Marcin Panasiuk <m.panasiuk@codetwo.com>
2022-02-08 11:16:12 +0100

    log

--- autorun.js
+++ autorun.js
@@ -1,13 +1,21 @@
 Office.initialize = function (reason) { };
 
 /**
+ * Handles the OnNewMessageCompose event.
+ */
+function onNewMessageComposeHandler(event) {
+    console.log("OnNewMessageCompose");
+}
+
+/**
  * Handles the OnMessageRecipientsChanged event.
  */
 function onMessageRecipientsChangedHandler(event) {
+    console.log("OnMessageRecipientsChanged");
 
     var signature = "<strong style='font-size: 25px;'> David Johnson </strong>";
-    console.log(`Setting signature to "${signature}".`);
     Office.context.mailbox.item.body.setSignatureAsync(signature, { coercionType: "html" }, function () { event.completed(); });
 }
 
+Office.actions.associate("onNewMessageComposeHandler", onNewMessageComposeHandler);
 Office.actions.associate("onMessageRecipientsChangedHandler", onMessageRecipientsChangedHandler);
