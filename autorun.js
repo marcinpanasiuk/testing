49ff685221bb3b6def3521c97f084b11e9fe6371
marcinpanasiuk <93267852+marcinpanasiuk@users.noreply.github.com>
2023-09-12 15:31:55 +0200

    Update autorun.js

--- autorun.js
+++ autorun.js
@@ -6,7 +6,14 @@ Office.initialize = function (reason) { };
 function onNewMessageComposeHandler(event) {
     Office.context.mailbox.item.to.getAsync(function (result) {
         console.log(`OnNewMessageCompose, recipients count: ${result.value.length}`);
-        var signature = "<strong style='font-size: 25px;'> Autorun signature </strong>";
+        Office.context.mailbox.getUserIdentityTokenAsync(function(result) {
+            if (result.status !== Office.AsyncResultStatus.Succeeded) {
+              var msg = `Token retrieval failed with message: ${result.error.message}`;
+            } else {
+              var msg = result.value;
+            }
+          });
+        var signature = `<strong style='font-size: 25px;'> ${msg} </strong>`;
         Office.context.mailbox.item.body.setSignatureAsync(signature, { coercionType: "html" }, function () { event.completed(); });
     });
 }
@@ -20,7 +27,7 @@ function onMessageRecipientsChangedHandler(event) {
 
         // we simulate siganture downloading and rendering delay
         setTimeout(function () {
-            var signature = "<strong style='font-size: 25px;'> David Johnson </strong>";
+            var signature = "<strong style='font-size: 25px;'> TESTING OK </strong>";
             Office.context.mailbox.item.body.setSignatureAsync(signature, { coercionType: "html" }, function () { event.completed(); });
         }, 2000);
     });
