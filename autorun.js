cd21c68c7e0fa07396b28cf99068be855beb2747
Marcin Panasiuk <m.panasiuk@codetwo.com>
2024-02-15 13:10:15 +0100

    formatting

--- autorun.js
+++ autorun.js
@@ -1,7 +1,7 @@
 Office.initialize = function () { };
 
 function onNewMessageComposeHandler(event) {
-    Office.context.mailbox.item.body.prependAsync("onNewMessageCompose fired", function () {
+    Office.context.mailbox.item.body.prependAsync("onNewMessageCompose fired\n\n", function () {
 
         try {
             Office.context.mailbox.item.notificationMessages.replaceAsync("addin-message", {
@@ -13,7 +13,7 @@ function onNewMessageComposeHandler(event) {
                 if (result.status !== Office.AsyncResultStatus.Succeeded) {
                     let message = result.error.name + ": " + result.error.message;
                     console.error(message);
-                    Office.context.mailbox.item.body.prependAsync("failed: " + message, function () {
+                    Office.context.mailbox.item.body.prependAsync("failed: " + message + "\n\n", function () {
                         event.completed();
                     });
                 }
@@ -23,7 +23,7 @@ function onNewMessageComposeHandler(event) {
         catch (error) {
             let message = result.error.name + ": " + result.error.message;
             console.error(message);
-            Office.context.mailbox.item.body.prependAsync("exception: " + message, function () {
+            Office.context.mailbox.item.body.prependAsync("exception: " + message + "\n\n", function () {
                 event.completed();
             });
         }
@@ -31,19 +31,19 @@ function onNewMessageComposeHandler(event) {
 }
 
 function onMessageComposeHandler(event) {
-    Office.context.mailbox.item.body.prependAsync("onMessageCompose fired", function () {
+    Office.context.mailbox.item.body.prependAsync("onMessageCompose fired\n\n", function () {
         event.completed();
     });
 }
 
 function onMessageRecipientsChangedHandler(event) {
-    Office.context.mailbox.item.body.prependAsync("onNewMessageCompose fired", function () {
+    Office.context.mailbox.item.body.prependAsync("onNewMessageCompose fired\n\n", function () {
         event.completed();
     });
 }
 
 function OnMessageFromChangedHandler(event) {
-    Office.context.mailbox.item.body.prependAsync("OnMessageFromChanged fired", function () {
+    Office.context.mailbox.item.body.prependAsync("OnMessageFromChanged fired\n\n", function () {
         event.completed();
     });
 }
