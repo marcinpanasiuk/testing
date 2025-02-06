dfb1b1cd601f16625afe70683f5195a7a49c1b24
marcinpanasiuk <93267852+marcinpanasiuk@users.noreply.github.com>
2025-02-06 15:01:13 +0100

    Update autorun.js

--- autorun.js
+++ autorun.js
@@ -15,9 +15,9 @@ function insertSignature(event) {
         <td style="font-size:9pt; font-family:Arial; width:400px; padding:0 0 5px 5px; border-left:#0099cc 2px solid" valign="top">
           <strong style="font-size:12pt; color:#0099cc">Your Company</strong><br>
           <span style="color:#0099cc">p:</span> <a style="text-decoration:none; color:#3c3c3b" href="tel:425-555-0100">425-555-0100</a><br>
-          <span style="color:#0099cc">a:</span> <span style="color:#3c3c3b">22 Branding Blvd, Azure Hill, NV, 89404, USA</span><br>
+          <span style="color:#0099cc">a:</span> <font color="#3d3c3f">22 Branding Blvd, Azure Hill, NV, 89404, USA</font><br>
           <span style="color:#0099cc">w:</span> <a style="text-decoration:none; color:#3c3c3b" href="http://www.yourdomain.url/">www.yourdomain.url</a><br>
-          <font color:"#0099cc">e:</font> <a style="text-decoration:none; color:#0099cc" href="mailto:admin@M365x84368197.OnMicrosoft.com">admin@M365x84368197.OnMicrosoft.com</a>
+          span style="color:#0099cc"e:</span> <a style="text-decoration:none; color:#0099cc" href="mailto:admin@M365x84368197.OnMicrosoft.com">admin@M365x84368197.OnMicrosoft.com</a>
         </td>
       </tr>
     </tbody>
